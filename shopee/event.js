d44c30aae6f72ba9bba359c59dd2f58de7dc3fe2
MDHUNG <92449546+MDHUNG@users.noreply.github.com>
2022-11-24 16:44:04 +0700

    update file

--- shopee/event.js
+++ shopee/event.js
@@ -1,2 +1,26 @@
 const downlaodApp = document.querySelector('.ctn-hover');
-console.log(downlaodApp)
+const QrApp = document.querySelector('.QRApp');
+
+downlaodApp.addEventListener('mouseover',addDisplayApp)
+function addDisplayApp () {
+  QrApp.style.display = 'flex';
+}
+
+downlaodApp.addEventListener('mouseout',removeDisplayApp)
+function removeDisplayApp () {
+  QrApp.style.display = 'none';
+}
+
+const language = document.querySelector('.nav-language');
+const languageOptions = document.querySelector('.language');
+
+language.addEventListener('mouseover', addDisplayLang)
+function addDisplayLang(){
+  languageOptions.style.display = 'block';
+}
+
+language.addEventListener('mouseout', removeDisplayLang)
+function removeDisplayLang(){
+  languageOptions.style.display = 'none';
+}
+
