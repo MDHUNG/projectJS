3882cbbf1fe913025c41a4120f19fcb1dbc62249
MDHUNG <92449546+MDHUNG@users.noreply.github.com>
2022-12-05 17:16:51 +0700

    update signUp

--- shopee/singUp/signUp.js
+++ shopee/singUp/signUp.js
@@ -12,76 +12,93 @@ for (let i = 0; i < showPass.length; i++){
   })
 }
 
-const emailsInput = document.getElementById("email");
-const messEmail = document.querySelector('.mess-email');
-const iconCheck = document.querySelector('.fa');
-const obj = {
-  email:"",
-  passWord:"",
-  rePassword:""
-};
-
-const validateEmail = (email) => {
-  return email.match(
-    /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-  );
-};
-
-const validateEmailValue = function () {
-  if (validateEmail(obj.email)){
-    iconCheck.style.display = 'block';
-    messEmail.style.display = 'none';
-    return (true)
-  } else{
-    messEmail.style.display = 'block';
-    messEmail.innerHTML = 'vui lòng nhập email ';
-    iconCheck.style.display = 'none';
-    messEmail.style.color = 'red';
-    return (false)
+function saveUsers (){
+  let emails ="";
+  let password ="";
+  let rePass = "";
+  //select email
+  const emailInput = document.getElementById('email');
+  emailInput.addEventListener('change', showEmail)
+  function showEmail (e){
+    emails = e.target.value;
   }
-}
 
-emailsInput.addEventListener('change',(e) => {
-  obj.email = e.target.value;
-})
+  //check email
+  const messEmail = document.querySelector('.mess-email');
+  const iconCheck = document.querySelector('.fa');
+  const validateEmail = (email) => {
+    return email.match(
+      /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    );
+  };
 
-const passWords =document.getElementById('pass');
-passWords.addEventListener('change',(e) => {
-  obj.passWord = e.target.value; 
-})
-
-const rePass = document.getElementById('rePass');
-rePass.addEventListener('change',(e) => {
-  obj.rePassword = e.target.value;
-})
-
-const messRePass = document.querySelector('.mess-repass'); 
-function validatePass ( pass, rePass){
-  if (pass === rePass){
-    messRePass.style.display = 'none'; 
-    return true;
-  }else {
-    messRePass.style.display = 'block';
-    messRePass.innerHTML ="nhập lại ";
-    messRePass.style.color ='red';
-    return false;
-  }
-}; 
-
- function validateSignUp (){
-  if (validatePass(obj.passWord,obj.rePassword) === true && validateEmailValue() === true){
-    return true
-  } else {
-    return false
-  }
-}
-
-const signUpBtn = document.querySelector('.sign-up-btn');
-signUpBtn.addEventListener('click',() => {
-  validatePass(obj.passWord,obj.rePassword)
-  if(validateSignUp() === true){
-    function postAccApi (){
-      
+  function validate() {
+    if (validateEmail(emails)){
+      iconCheck.style.display = 'block';
+      messEmail.style.display = 'none';
+      return (true)
+    } else{
+      messEmail.style.display = 'block';
+      messEmail.innerHTML = 'vui lòng nhập email ';
+      iconCheck.style.display = 'none';
+      messEmail.style.color = 'red';
+      return (false)
     }
   }
-})
+  
+  const passwordInput = document.getElementById('pass');
+  passwordInput.addEventListener('change', showPass)
+  function showPass(e) {
+    password = e.target.value;
+  }
+  
+  const repass = document.getElementById('rePass');
+  repass.addEventListener('change', validatePass)
+  function validatePass (e) {
+    rePass = e.target.value;
+  }
+
+  const Btn = document.querySelector('.sign-up-btn');
+  Btn.addEventListener('click', addUsers)
+  function addUsers (){
+    
+    // check password
+    const messRePass = document.querySelector('.mess-repass'); 
+    function validatePass ( pass, rePass){
+      if (pass === rePass){
+        messRePass.style.display = 'none'; 
+        return true;
+      } else {
+        messRePass.style.display = 'block';
+        messRePass.innerHTML ="nhập lại ";
+        messRePass.style.color ='red';
+        return false;
+      }
+    }; 
+    
+    if (validate() == true && validatePass( password, rePass) == true){
+      let users =
+        {
+          email: emails,
+          password: password,
+        }
+
+      if (users == null){
+        users = [];
+      }
+      fetch('http://localhost:3000/Users',{
+        method:'post',
+        headers:{
+          'content-type': 'application/json',
+        },
+        body: JSON.stringify(users),
+      })
+      .then((response) => response.json())
+      .then((users) => {
+        console.log(users);
+      })
+    }
+  }
+
+};
+saveUsers()
